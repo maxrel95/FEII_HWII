37eccdc625eacae741998c09989b2964652f07e0
Maxime Borel <58341766+maxrel95@users.noreply.github.com>
2023-02-10 10:26:54 +0100

    Update GMM.py

--- GMM.py
+++ GMM.py
@@ -27,6 +27,8 @@ df.dropna( axis=0, inplace=True )
 exog_df = df[ [ 'Rt', 'Rft', 'ct' ] ]
 instrument_df = df[ [ 'r_lag1', 'r_lag2', 'rf_lag1', 'rf_lag2', 'ct_lag1',
                            'const']]
+#instrument_df = df[ [ 'const', 'r_lag1', 'rf_lag1', 'ct_lag1' ] ]
+#instrument_df = df[ [ 'const', 'r_lag1', 'r_lag2', 'rf_lag1', 'rf_lag2', 'ct_lag1', 'ct_lag2' ] ]
 
 exog, instrument  = map( np.asarray, [ exog_df, instrument_df ] )
 instrument_augmented = np.kron( np.ones( ( 2, 1 ) ), instrument )
@@ -43,10 +45,15 @@ def moment_condition( params, exog ):
     return err1
 
 endog1 = np.zeros( exog.shape[ 0 ] )   
-mod1 = gmm.NonlinearIVGMM( endog1, exog, instrument_augmented, moment_condition )
+mod1 = gmm.NonlinearIVGMM( endog1, exog, instrument_augmented, moment_condition, k_moms=8, k_params=2 )
 
 w0inv = instrument.T @ instrument / len( endog1 )
 param, weights = mod1.fititer( [ 1, -1 ] )
 res1 = mod1.fit( [ 1, -1 ], maxiter=100 ) 
 print( res1.summary( yname='Euler Eq', xname=[ 'discount', 'CRRA' ] ) )
 
+
+(df[ 'Rt' ]-1).expanding().mean()
+((df[ 'Rt' ]-1)**2).expanding().mean()
+((df[ 'Rt' ]-1)**3).expanding().mean()
+((df[ 'Rt' ]-1)**4).expanding().mean()
