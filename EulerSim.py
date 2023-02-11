c9ee12b14b69be7c24e28112f84854d5975bea83
maxrel95 <58341766+maxrel95@users.noreply.github.com>
2023-02-11 10:46:50 +0100

    comment + plot sub

--- EulerSim.py
+++ EulerSim.py
@@ -11,6 +11,7 @@ import statsmodels.api as sm
 
 
 # Question 1 
+# parameter of the process
 b = np.array( [ .003, .022 ] ).T
 A = np.array( [[ .073, .620],
                 [ .015, -.122]])
@@ -25,8 +26,8 @@ Om = np.array( [
 ])
 
 n_sim = 500
-beta = .95
-tau = 2
+beta = .95 # discount factor
+tau = 2 # risk aversion 
 tol = .0001
 max_iter = 1000
 
@@ -37,23 +38,23 @@ P = 3
 r = Om.shape[ 0 ]
 Omi = inv( Om )
 B = cholesky( Omi,  ).T
-B @ Om @ B.T
+B @ Om @ B.T # check if implementation give identity matrix
 Bi = inv( B )
-F = B @ A @ Bi
+F = B @ A @ Bi # F in equation (27) of slides
 
 X = np.eye( r**2 ) - np.kron( F, F )
 Xi = inv( X ) 
 SigSt = ( Xi @ np.eye( r ).reshape( [ r**2, 1 ] ) ).reshape( [ r, r ]) 
-z1L = -P * np.sqrt( SigSt[ 0, 0 ] )
+z1L = -P * np.sqrt( SigSt[ 0, 0 ] ) # defin the span of the innovation 
 z1U = P * np.sqrt( SigSt[ 0, 0 ] )
 z2L = -P * np.sqrt( SigSt[ 1, 1 ] )
 z2U = P * np.sqrt( SigSt[ 1, 1 ] )
 NvL = Nv + 1
-dz1 = ( z1U - z1L ) / ( NvL[ 0 ] - 1 )
+dz1 = ( z1U - z1L ) / ( NvL[ 0 ] - 1 ) # increment of the zt 
 dz2 = ( z2U - z2L ) / ( NvL[ 1 ] - 1 )
 z1BG = np.arange( z1L, z1U+dz1, dz1 ).T
 z2BG = np.arange( z2L, z2U+dz2, dz2 ).T
-z1g = ( z1BG[ 1: ] + z1BG[ :-1 ] ) / 2
+z1g = ( z1BG[ 1: ] + z1BG[ :-1 ] ) / 2 # midpoint
 z2g = ( z2BG[ 1: ] + z2BG[ :-1 ] ) / 2
 z1BG[ 0 ] = -np.inf # lower and upper bnd for the markov chain
 z1BG[ -1 ] = np.inf
@@ -71,6 +72,7 @@ imA = inv( np.eye( r ) - A )
 yg = zg @ Bi.T + np.kron( b.reshape( [ 1 , -1 ] ) @ imA.T, np.ones( ( Ns, 1 ) ) )
 PiM = np.zeros( ( Ns, Ns ) )
 
+# compute the transition probability matrix
 for s_idx in range( Ns ):
     ztm1 = zg[ s_idx, : ].reshape( [ -1, 1 ] )
     f = F @ ztm1
@@ -87,21 +89,22 @@ for s_idx in range( Ns ):
 H = np.ones( ( Ns, 1 ) )
 is_tol = 0
 iter = 0
-eyg = np.exp( yg )
+eyg = np.exp( yg ) # first col is dividend, 2nd col is consumption
 
 while ( ( is_tol < 1 ) & ( iter < max_iter ) ):
     eyg1 = eyg[ :, 0 ].reshape( [ -1, 1 ] )
     eyg2 = eyg[ :, 1 ].reshape( [ -1, 1 ] )
     
     X = ( 1 + H ) * eyg1 * ( eyg2**( -tau ) )
-    Hn = beta * PiM @ X
+    Hn = beta * PiM @ X # eq 29
     
     if np.max( np.abs( Hn - H ) ) < tol:
         is_tol = 1
     
     iter += 1
-    print( iter )
     H = Hn
+
+print( iter )
 
 rH = H.reshape( [ Nv[ 0 ], Nv[ 0 ] ] ).T
 x, y = np.meshgrid( np.arange( 10 ), np.arange( 10 ) )
@@ -126,11 +129,11 @@ res = np.zeros( ( n_sim, 4 ) )
 for sim_ctr in range( n_sim ):
     Auxpi = cumPim[ int( s ), : ] < ( usim[ sim_ctr ] * np.ones( ( 1, PiM.shape[ 1 ] ) ) )
     sn = np.sum( Auxpi )
-    Rt = ( 1 + H[ sn ] ) / H[ int( s ) ] * eyg[ sn, 0 ]
-    res[ sim_ctr, : ] = np.hstack( [ eyg[ sn, : ], Rt, H[ sn ] ] )
+    Rt = ( 1 + H[ sn ] ) / H[ int( s ) ] * eyg[ sn, 0 ] #compute rate of return risky
+    res[ sim_ctr, : ] = np.hstack( [ eyg[ sn, : ], Rt, H[ sn ] ] ) # div, cons, ret, p/d
     s = sn
 
-plt.figure(2, figsize=(15, 12))
+plt.figure(2, figsize=(20, 16))
 plt.subplot( 4, 1, 1 )
 plt.plot( res[ :, 0 ] )
 plt.title( 'dividend growth' )
@@ -139,17 +142,19 @@ plt.plot( res[ :, 1 ] )
 plt.title( 'Consumption growth' )
 plt.subplot( 4, 1, 3 )
 plt.plot( res[ :, 2 ] )
+plt.title( 'Rate of return of risk asset' )
 plt.subplot( 4, 1, 4 )
 plt.plot( res[ :, 3 ] )
+plt.title( 'Price/dividend ratio' )
 plt.savefig( 'results/subplots.png' )
 plt.show()
 
 print( 'Dynamic for dividend growht rate' )
-y = np.log( res[ 1:, 0 ] )
+y = np.log( res[ 1:, 0 ] ) #VAR of dividend growth 
 T = y.shape[ 0 ] 
-x = np.log( res[ :-1, :2 ] )
+x = np.log( res[ :-1, :2 ] ) # previous div and conso
 x = sm.add_constant( x )
-mdl = sm.OLS( y, x )
+mdl = sm.OLS( y, x ) # check if it corresponds with A and b above
 result = mdl.fit()
 print( result.summary().as_latex() )
 
